448962dfecd66110ab8071c85e6b778411642df7
thangnncps14579 <77538291+thangnncps14579@users.noreply.github.com>
2021-11-29 10:30:48 +0700

    commit file pom

--- src/main/java/com/unknownshop/form/DialogWelcome.java
+++ src/main/java/com/unknownshop/form/DialogWelcome.java
@@ -3,17 +3,11 @@ package com.unknownshop.form;
 import com.unknownshop.main.MainForm;
 import com.unknownshop.util.XPanel;
 
-public class DialogWelcome extends javax.swing.JDialog {
+public final class DialogWelcome extends javax.swing.JDialog {
 
     public DialogWelcome() {
         initComponents();
-        XPanel.dlgWelcome = this;
-        new Thread(){
-            @Override
-            public void run(){
-                new MainForm().setVisible(true);
-            }
-        }.start();
+        init();
     }
 
     @SuppressWarnings("unchecked")
@@ -21,30 +15,32 @@ public class DialogWelcome extends javax.swing.JDialog {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        lblAo = new javax.swing.JLabel();
+
+        jLabel1.setText("jLabel1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setAutoRequestFocus(false);
+        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         setUndecorated(true);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/welcome.gif"))); // NOI18N
-        jLabel1.setBorder(javax.swing.BorderFactory.createMatteBorder(3, 3, 3, 3, new java.awt.Color(51, 51, 51)));
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jLabel1))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 280, Short.MAX_VALUE)
-        );
+        lblAo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Shop.gif"))); // NOI18N
+        jPanel1.add(lblAo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 500, 490));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 500, 490));
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -68,17 +64,40 @@ public class DialogWelcome extends javax.swing.JDialog {
             java.util.logging.Logger.getLogger(DialogWelcome.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        
+        
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 DialogWelcome dialog = new DialogWelcome();
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
                 dialog.setVisible(true);
             }
         });
     }
 
+    Thread t;
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lblAo;
     // End of variables declaration//GEN-END:variables
+
+    private void init(){
+        XPanel.dlgWelcome = this;
+        new Thread(){
+            @Override
+            public void run(){
+                new MainForm().setVisible(true);
+            }
+        }.start();
+    }
 }

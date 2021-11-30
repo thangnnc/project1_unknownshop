fc5864c0bda03428c9bdabbb56682fa22343c1e0
hoangduy2812002 <88256150+hoangduy2812002@users.noreply.github.com>
2021-11-30 12:59:31 +0700

    chinh lai ham update san pham

--- src/main/java/com/unknownshop/form/admin/PanelProductManager.java
+++ src/main/java/com/unknownshop/form/admin/PanelProductManager.java
@@ -1248,6 +1248,8 @@ public class PanelProductManager extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Phương thức lấy thông tin trên form">  
     private Products getForm(){
         Products product = new Products();
+        int id = (int) tblDanhSachSP.getValueAt(this.row, 1);
+        product.setId(id);
         product.setName(txtTenSP.getText());
         product.setPrice(Float.parseFloat(txtGiaSP.getText()));
         product.setQuantity(Integer.valueOf(txtSoLuong.getText()));
